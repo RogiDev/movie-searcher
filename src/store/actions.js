5cb4b282c20dd7ab40f647021957d5405d1d21ed
RogiDev <31067797+RogiDev@users.noreply.github.com>
2019-01-12 17:16:03 +0200

    backup

--- src/store/actions.js
+++
@@ -1,7 +0,0 @@
-export const SEARCH_MOVIES = 'SEARCH_MOVIES';
-export const POPULAR_MOVIES = 'POPULAR_MOVIES'
-export const CLEAN_SEARCH = 'CLEAN_SEARCH';
-export const SELECT_MOVIE = 'SELECT_MOVIE';
-export const SAVE_MOVIE = 'SAVE_MOVIE';
-export const UPDATE_MOVIE = 'UPDATE_MOVIE';
-export const ADD_MOVIE = 'ADD_MOVIE';
