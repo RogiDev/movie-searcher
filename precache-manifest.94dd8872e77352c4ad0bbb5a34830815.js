311b6032f4ca0687aa1e44f953f47f7ab337a7eb
RogiDev <31067797+RogiDev@users.noreply.github.com>
2019-01-23 21:16:20 +0200

    app

---
+++ precache-manifest.94dd8872e77352c4ad0bbb5a34830815.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "777f74208da8ac93fbdd",
+    "url": "/static/js/main.777f7420.chunk.js"
+  },
+  {
+    "revision": "4a2a2303cbfd02e5399a",
+    "url": "/static/js/1.4a2a2303.chunk.js"
+  },
+  {
+    "revision": "777f74208da8ac93fbdd",
+    "url": "/static/css/main.a343e40f.chunk.css"
+  },
+  {
+    "revision": "feaa7d94e487f549496e7c0379b80385",
+    "url": "/index.html"
+  }
+];
